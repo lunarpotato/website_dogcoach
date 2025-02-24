edb02a231c73038957ab16b19e5a7f59e6396f8e
lunarpotato <105643123+lunarpotato@users.noreply.github.com>
2025-02-24 12:01:53 +0100

    file structure updated, layout adjustments regarding form content

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
-import Image from "next/image";
-import Form from "./form"
-
 export default function Home() {
-  return (
-
-    
-    <Form/>
-
-
-
-  );
-}
+    return (
+  
+      <main>
+      <h2>Landing Page</h2>
+      </main>
+  
+  
+  
+    );
+  }
+  
