c91c92251507d4b4d56a71b3795cae924b5627b9
lunarpotato <chantal.hann@gmail.com>
2025-03-04 19:26:06 +0100

    module.css für navbar und kontaktformular eingebaut

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,12 +1,33 @@
 import Link from "next/link";
+import Style from './Navbar.module.css';
 
 export default function Navbar() {
     return (
-        <nav>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/contact">Kontakt</Link>
-        </nav>
+          
+    <nav className={`navbar navbar-expand-lg ${Style.nav_body}`}>
+        <div className="container-fluid">
+
+            <div className="container">
+    
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <Link className="nav-link active" href="/">Home</Link> 
+                    </li>
+
+                    <li className="nav-item">
+                        <Link className="nav-link" href="/about">About</Link>
+                    </li>
+
+                    <li className="nav-item">
+                        <Link className="nav-link" href="/contact">Kontakt</Link>
+                    </li>
+            
+                </ul> 
+            </div> 
+
+        </div>
+
+    </nav>
 
     )
 }
