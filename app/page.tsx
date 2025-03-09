6310a18f480ba12050a98527edaf9f446adb5a3e
lunarpotato <chantal.hann@gmail.com>
2025-03-09 21:24:22 +0100

    bootstrap snippets tests for header and welcome text

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
 export default function Home() {
     return (
       <main>
-        <div className="container">
-          <h3>Landing Page</h3>
+        <div className="container-fluid p-5 bg-body-tertiary">
+         
+              <div className="container text-start">
+                <h3>Weil Beziehung mehr ist als Erziehung.</h3>
+                <p className="opacity-75">
+                Individuelles Coaching für ein harmonisches Mensch-
+                Hund-Team.
+                </p>
+                <p>
+                  <a className="btn btn-lg btn-dark" href="#">
+                    Kontakt
+                  </a>
+                </p>
+              </div>
+          
         </div>
+
+        <>
+        <div className="container">
+          <img src="/" className="rounded float-start" alt="dog_first" />
+          <img src="/" className="rounded float-end" alt="dog_scnd" />
+          </div>
+        </>
+
+
+        
+
       </main>
   
     );
