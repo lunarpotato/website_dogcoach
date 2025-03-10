6d6ebeaa1dc2d3f0cc156e0a1d5375d5c96a7dce
lunarpotato <chantal.hann@gmail.com>
2025-03-10 19:01:11 +0100

    textblocks in header created and layout adjusted

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,21 @@
 export default function Home() {
     return (
       <main>
-        <div className="container-fluid p-5 bg-body-tertiary">
-         
-              <div className="container text-start">
-                <h3>Weil Beziehung mehr ist als Erziehung.</h3>
-                <p className="opacity-75">
-                Individuelles Coaching für ein harmonisches Mensch-
-                Hund-Team.
-                </p>
-                <p>
-                  <a className="btn btn-lg btn-dark" href="#">
-                    Kontakt
-                  </a>
-                </p>
-              </div>
-          
-        </div>
-
         <>
         <div className="container">
-          <img src="/" className="rounded float-start" alt="dog_first" />
-          <img src="/" className="rounded float-end" alt="dog_scnd" />
+          <div className="hstack gap-3 align-items-stretch" style={{ height: "250px" }}>
+            <div 
+            className="h-auto w-auto mw-25 p-5 bg-body-secondary border align-self-start" style={{ maxWidth: "350px"}}>
+              Weil Beziehung mehr ist als Erziehung.
+            </div>
+            
+            <div className=" ms-auto h-auto w-auto mw-25 p-5 bg-body-secondary border align-self-end" style={{ maxWidth: "350px", wordBreak: "break-word"}}>
+              Individuelles Coaching für ein harmonisches Mensch-Hund-Team.
+            </div>
           </div>
+        </div>
+
         </>
-
-
-        
 
       </main>
   
