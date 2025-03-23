449869974cd66d21b746b8fa0e5412735d15de6c
DESKTOP-NE5JOVO\dania <danial.nowak@gmx.de>
2025-03-23 21:48:41 +0100

    navbar und Dropdown angepasst

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -7,13 +7,18 @@ import DropdownMenu from "react-bootstrap/DropdownMenu";
 import DropdownItem from "react-bootstrap/DropdownItem";
 
 export default function Navbar() {
+
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
+
   return (
     <nav className={`navbar navbar-expand-lg   ${Style.nav_body}`}>
       <div className="container-fluid">
-       
+
         {/* Navbar-Toggler für Mobile-Version*/}
         <div className="container d-flex justify-content-end btn-group dropstart">
-      
+
           {/* Hauptnavigation */}
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <ul className="nav me-auto mb-2 mb-lg-0">
@@ -36,29 +41,27 @@ export default function Navbar() {
               </li>
             </ul>
           </div>
-            <Dropdown className='d-lg-none'>
-                <Dropdown.Toggle 
-                    bsPrefix="toggle"
-                    variant="" 
-                    id="dropdown-basic"
-                    className={Style['navbar-toggler']}
-                    // className='navbar-toggler'
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNavAltMarkup"
-                    aria-controls="navbarNavAltMarkup"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </Dropdown.Toggle>
+          <Dropdown className='d-lg-none' show={dropdownOpen} onToggle={toggleDropdown}>
+            <Dropdown.Toggle
+              bsPrefix="toggle"
+              variant=""
+              id="dropdown-basic"
+              className={Style['navbar-toggler']}
+              type="button"
+              aria-controls="navbarNavAltMarkup"
+              aria-expanded={dropdownOpen ? 'true' : 'false'}
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </Dropdown.Toggle>
 
-                <Dropdown.Menu  className={Style['dropdown-menu']}>
-                <Dropdown.Item as={Link} href="/" className={Style['dropdown-item']}>Home</Dropdown.Item>
-                <Dropdown.Item as={Link} href="/about">About</Dropdown.Item>
-                <Dropdown.Item as={Link} href="/contact">Kontakt</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
-           
+            <Dropdown.Menu className={Style['dropdown-menu']}>
+              <Dropdown.Item as={Link} href="/" className={Style['dropdown-item']}>Home</Dropdown.Item>
+              <Dropdown.Item as={Link} href="/about" className={Style['dropdown-item']}>About</Dropdown.Item>
+              <Dropdown.Item as={Link} href="/contact" className={Style['dropdown-item']}>Kontakt</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+
         </div>
       </div>
     </nav>
