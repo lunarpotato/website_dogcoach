c5ac350ea88555dac7f88084ed9ea69bfebc0f51
lunarpotato <chantal.hann@gmail.com>
2025-03-23 20:21:06 +0100

    set up navbar toggler menue and button

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,41 +1,68 @@
+"use client"
+import React, { useState } from 'react';
 import Link from "next/link";
-import Style from './Navbar.module.css';
+import Style from "./Navbar.module.css";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownMenu from "react-bootstrap/DropdownMenu";
+import DropdownItem from "react-bootstrap/DropdownItem";
 
 export default function Navbar() {
-    return (
-          
+  return (
     <nav className={`navbar navbar-expand-lg   ${Style.nav_body}`}>
-        <div className="container-fluid">
+      <div className="container-fluid">
+       
+        {/* Navbar-Toggler für Mobile-Version*/}
+        <div className="container d-flex justify-content-end btn-group dropstart">
+      
+          {/* Hauptnavigation */}
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <ul className="nav me-auto mb-2 mb-lg-0">
+              <li className="nav-link">
+                <Link className={Style["nav-link"]} href="/">
+                  Home
+                </Link>
+              </li>
 
-            <div className="container d-flex justify-content-end">
-                <button className="navbar-toggler" 
-                    type="button" data-bs-toggle="collapse" 
-                    data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+              <li className="nav-link">
+                <Link className={Style["nav-link"]} href="/about">
+                  About
+                </Link>
+              </li>
 
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup"> 
-                          
-                    <ul className="nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-link">
-                            <Link className={Style['nav-link']} href="/">Home</Link> 
-                        </li>
+              <li className="nav-link">
+                <Link className={Style["nav-link"]} href="/contact">
+                  Kontakt
+                </Link>
+              </li>
+            </ul>
+          </div>
+          
+          {/* Dropdown-Menue*/}
 
-                        <li className="nav-link">
-                            <Link className={Style['nav-link']} href="/about">About</Link>
-                        </li>
+            <Dropdown className="dropend d-lg-none">
+                <Dropdown.Toggle 
+                    bsPrefix="toggle"
+                    variant="" 
+                    id="dropdown-basic"
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </Dropdown.Toggle>
 
-                        <li className="nav-link">
-                            <Link className={Style['nav-link']} href="/contact">Kontakt</Link>
-                        </li>
-                
-                    </ul> 
-                </div>
-            </div> 
-
+                <Dropdown.Menu  style={{backgroundColor: "#bfd59ec4"}}>
+                <Dropdown.Item as={Link} href="/">Home</Dropdown.Item>
+                <Dropdown.Item as={Link} href="/about">About</Dropdown.Item>
+                <Dropdown.Item as={Link} href="/contact">Kontakt</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+           
         </div>
-
+      </div>
     </nav>
-
-    )
+  );
 }
