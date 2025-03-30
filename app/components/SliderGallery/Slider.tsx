753ec1a64757d31031a3dfbe4b89b7463b240d3d
lunarpotato <chantal.hann@gmail.com>
2025-03-30 19:10:07 +0200

    next-image durch bootrstrap image erstetzt im slider

--- app/components/SliderGallery/Slider.tsx
+++ app/components/SliderGallery/Slider.tsx
@@ -1,40 +1,28 @@
 import React from "react";
 import { Carousel, CarouselItem } from "react-bootstrap";
-import Image from "next/image";
+import Image from "react-bootstrap/Image";
 import Style from "../SliderGallery/Slider.module.css";
 
 export default function Slider() {
-  const _width = 0;
-  const _height = 0;
 
   return (
     <div className={Style.container}>
       <Carousel>
         <CarouselItem>
           <Image
-            src={"/img/dog_backpack.jpg"}
-            alt="First Slide"
-            layout="responsive"
-            width={_width}
-            height={_height}
+            src="/img/dog_backpack.jpg" fluid
+         
           />
         </CarouselItem>
         <CarouselItem>
           <Image
-            src="/img/forest_walk.jpg"
-            alt="Second Slide"
-            layout="responsive"
-            width={_width}
-            height={_height}
+            src="/img/forest_walk.jpg" fluid
           />
         </CarouselItem>
         <CarouselItem>
           <Image
-            src="/img/paws_hands.jpg"
-            alt="Second Slide"
-            layout="responsive"
-            width={_width}
-            height={_height}
+            src="/img/paws_hands.jpg" fluid
+           
           />
         </CarouselItem>
       </Carousel>
