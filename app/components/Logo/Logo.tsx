be54c42c034ffce5f985ab442a6cc9fa4f2f2a79
DESKTOP-NE5JOVO\dania <danial.nowak@gmx.de>
2025-04-05 16:02:22 +0200

    prettier installiert, settings justiert, logo-komponente angepasst

--- app/components/Logo/Logo.tsx
+++ app/components/Logo/Logo.tsx
@@ -1,20 +1,39 @@
-"use client"
+"use client";
 
-import { useRouter } from 'next/navigation'
-import React from 'react'
+import { useRouter } from "next/navigation";
+import React from "react";
 
-
-export default function Logo({ _url, _className = "", _src }: {_url: string; _className?: string; _src: string;}) {
+export default function Logo({
+  _url,
+  _className = "",
+  _src,
+  _width = 55,
+  _height = 55,
+  _alt,
+}: {
+  _url: string;
+  _className: string;
+  _src: string;
+  _width?: number;
+  _height?: number;
+  _alt: string;
+}) {
   const router = useRouter();
 
   return (
-
-    <img  onClick={() => router.push(_url)} className={_className} width={55} height={55} src= {_src} alt=""/>
-  
-  )
+    <img
+      onClick={() => router.push(_url)}
+      className={_className}
+      width={_width}
+      height={_height}
+      src={_src}
+      alt=""
+    />
+  );
 }
 
- 
-{/* <li className= {Style["logo"]}>
+{
+  /* <li className= {Style["logo"]}>
 <img width={55} height={55} src="../img/Logo.svg" ></img>  
-</li> */}
+</li> */
+}
