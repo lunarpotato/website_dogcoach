ef24566b16e4977ba490ae3d4cb510dac7be83cf
lunarpotato <105643123+lunarpotato@users.noreply.github.com>
2025-04-07 15:06:05 +0200

    angepasstes logo eingebunden, logo bei "leistungen" seite anzeigen lassen, navbarfarbe geändert

--- app/leistungen/page.tsx
+++ app/leistungen/page.tsx
@@ -1,7 +1,22 @@
+"use client"
+import Image from "next/image"
+import logoImage from "@/public/img/Logo.svg"
+import { relative } from "path";
+
 export default function Leistungen() {
     return (
         <main>
-            <div className='container'>
+            <div className='container' style={{position:"relative"}}>
+            <Image src={logoImage} 
+                placeholder="blur"
+                quality={50}
+                className="img-fluid"
+                style={{ height: "75%", maxHeight: "600px", objectFit: "contain"}}
+                alt="Logo Team Hund-Mensch"
+                blurDataURL="@/public/img/Logo.svg"
+                >
+                        
+            </Image>
             </div>
         </main>
 
