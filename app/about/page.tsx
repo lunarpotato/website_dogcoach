cdd02ae9a41960e370b846f1cc7f359423e13047
lunarpotato <105643123+lunarpotato@users.noreply.github.com>
2025-04-07 16:57:35 +0200

    text und layout bei about/leistungen angepasst, die body-background-color in ein gradient geändert (wechseln evtl)

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,16 +3,21 @@ import Image from "next/image";
 export default function About() {
   return (
     <main>
-      <div className="container">
+      <div className="container container-custom">
         <div className="row featurette">
           <div className="col-md-7">
-            <h2 className="featurette-heading fw-normal lh-1">
-              Über mich {""}
-              <span className="text-body-secondary">Hallo Tine</span>
-            </h2>
-            <p className="lead">
-              Some great placeholder content for the first featurette here.
-              Imagine some exciting prose here.
+            <h1 className="featurette-heading fw-normal lh-1 custom-heading">
+              Über mich{""}
+          
+            </h1>
+            <p className="lead lead-text" style={{wordBreak: "break-word"}}>
+            Das Wesen Hund begleitet und begeistert mich von Kindheit an. 
+            Als ich an meiner ehemaligen Arbeitsstelle meinen Hund im Umgang mit beeinträchtigten und auch
+            gesunden Menschen beobachtete, bekam ich die Idee und den Wunsch dieses Thema zu vertiefen. Ich
+            wollte mehr erfahren über das Verhalten der Hunde, über die Beziehung Mensch-Hund und wie wir uns
+            die Ressourcen von beiden Seiten zunutze machen können.
+            Ich lernte sehr viel über Kommunikation, über Wechselwirkungen und darüber, dass unser Begleiter
+            Hund uns Menschen immer wohlgesonnen ist und uns unterstützen möchte.
             </p>
           </div>
         </div>
--- app/leistungen/page.tsx
+++ app/leistungen/page.tsx
@@ -6,18 +6,42 @@ import { relative } from "path";
 export default function Leistungen() {
     return (
         <main>
-            <div className='container' style={{position:"relative"}}>
-            <Image src={logoImage} 
+            <div className='container container-custom' style={{position:"relative"}}>
+            
+        <div className="row featurette">
+          <div className="col-md-7">
+            <h1 className="featurette-heading fw-normal lh-1 custom-heading">
+              Angebot {""}
+          
+            </h1>
+            <p className="lead lead-text" style={{wordBreak: "break-word"}}>
+            2021 habe ich bei Antonia Conzett in Steg im Tösstal die Coachingausbildung begonnen. Die Ausbildung
+            ist abgeschlossen. Eine stete Weiterbildung und Austausch mit anderen Coaches begleiten mich und
+            halten mich auf dem neuesten Stand.
+            Artgerechte Kommunikation, Verständnis, Wertschätzung und Verbundenheit sind wichtige Begriffe auf
+            dem Weg zu einem entspannten Team Hund-Mensch.
+            Ich biete Einzelcoachings an, keine Gruppenstunden oder Rudelspaziergänge.
+
+            <br/>Preis: 70.-/h
+            </p>
+            <div className="col-md2">
+          <Image src={logoImage} 
                 placeholder="blur"
                 quality={50}
                 className="img-fluid"
-                style={{ height: "75%", maxHeight: "600px", objectFit: "contain"}}
+                style={{ height: "75%", maxHeight: "600px", objectFit: "contain", paddingBottom:"2em"}}
                 alt="Logo Team Hund-Mensch"
                 blurDataURL="@/public/img/Logo.svg"
+
                 >
                         
             </Image>
-            </div>
+          </div>
+          </div>
+        </div>
+     
+        
+        </div>
         </main>
 
     );
