18d95c11623cd61e0d272f6cd68f0e169c263d4a
lunarpotato <chantal.hann@gmail.com>
2025-04-20 09:34:48 +0200

    slider navigation am rand und pagination unten klickbar, slides befinden sich im loop

--- app/components/ImageSlider/ImageSlider.tsx
+++ app/components/ImageSlider/ImageSlider.tsx
@@ -1,24 +1,35 @@
 "use client";
-
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import Styles from "./ImageSlider.module.css";
+import { Navigation, Pagination } from "swiper/modules";
 
+//Styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+//Custom Styles
+import Styles from "./ImageSlider.module.css";
 
 export default function ImageSlider() {
   return (
-    <Swiper spaceBetween={50} slidesPerView={1}
-    navigation
-    pagination={{ clickable: true }}
-    scrollbar={{ draggable: true }}
-    onSlideChange={() => console.log('slide change')}
-    onSwiper={(swiper) => console.log(swiper)}>
-      <SwiperSlide>Lorem ipsum dolor sit amet, consetetur sadipscing elitr? sed diam voluptua. </SwiperSlide>
-      <SwiperSlide>Sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat?</SwiperSlide>
+    <Swiper
+      // direction="vertical"
+      loop={true}
+      navigation={true}
+      pagination={{ clickable: true }}
+      modules={[Navigation, Pagination]}
+      onSlideChange={() => console.log("slide change")}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr? sed diam
+        voluptua.{" "}
+      </SwiperSlide>
+      <SwiperSlide>
+        Sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+        aliquyam erat?
+      </SwiperSlide>
       <SwiperSlide>Invidunt ut labore et dolore magna aliquyam?</SwiperSlide>
     </Swiper>
   );
